beecd1055e604c9507cf7d361c73a810be2df8a3
luchoc <luiscarlin@gmail.com>
2016-09-07 00:27:35 -0400

    initial commit

---
+++ src/main/java/com/coursecreation/CourseCreationAppApplication.java
@@ -0,0 +1,12 @@
+package com.coursecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCreationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseCreationAppApplication.class, args);
+	}
+}
